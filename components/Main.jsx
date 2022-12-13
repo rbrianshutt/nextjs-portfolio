b958aa671d97cfd1adec889346c2862ac85e030b
rbrianshutt <rbrianshutt@gmai.com>
2022-12-12 20:39:30 -0600

    update main

--- components/Main.jsx
+++ components/Main.jsx
@@ -10,27 +10,32 @@ function Main() {
 			<div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
 				<div>
 					<p className='uppercase text-sm tracking-widest text-gray-600'>
-						LET'S BUILD SOMETHING TOGETHER
+						WELCOME TO MY PORTFOLIO
 					</p>
 					<h1 className='py-4 text-gray-700'>
 						Hi, I'm <span className='text-[#5651e5]'>Brian</span>
 					</h1>
-					<h1 className='py-2 text-gray-700'>A Front-End Web Developer</h1>
+					<h1 className='py-2 text-gray-700'>Developer/Software Engineer</h1>
 					<p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
 						I’m focused on building responsive front-end web applications integrating
 						back-end technologies.
 					</p>
 					<div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
 						<div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-							<Link href='https://www.linkedin.com/in/brian-shutt-28bb16177/'><FaLinkedinIn /></Link>
+							<Link href='https://www.linkedin.com/in/brian-shutt-28bb16177/'>
+								<FaLinkedinIn />
+							</Link>
 						</div>
 						<div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-							<Link href='https://github.com/rbrianshutt'><FaGithub /></Link>
+							<Link href='https://github.com/rbrianshutt'>
+								<FaGithub />
+							</Link>
 						</div>
 						<div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-							<Link href='mailto:richardbrianshutt@gmail.com'><AiOutlineMail /></Link>
+							<Link href='mailto:richardbrianshutt@gmail.com'>
+								<AiOutlineMail />
+							</Link>
 						</div>
-						
 					</div>
 				</div>
 			</div>
